b047040b7a71fd0002462b786a93a3864ab1423a
ameerdwdev <dawaraameer14@gmail.com>
2025-12-09 11:23:30 +0400

    finish relationship

--- database/migrations/2025_12_08_082554_create_orders_table.php
+++ database/migrations/2025_12_08_082554_create_orders_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('payment_id')->constrained('payments');
             $table->timestamps();
         });
     }
