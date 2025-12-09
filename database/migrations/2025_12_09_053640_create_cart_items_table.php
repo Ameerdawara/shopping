4d2bb97b20489c1c5a671c41aeaec5b0a773a2dd
ameerdwdev <dawaraameer14@gmail.com>
2025-12-09 10:35:39 +0400

    first in branch

--- database/migrations/2025_12_09_053640_create_cart_items_table.php
+++ database/migrations/2025_12_09_053640_create_cart_items_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('cart_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('cart_id')->constrained('cards');
             $table->timestamps();
         });
     }
