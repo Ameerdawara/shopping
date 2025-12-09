a7cce6489dd7460b6d868141d5a12da1bf33d18c
Ghaieth Kahel <ghaiethka@gmail.com>
2025-12-09 12:22:05 +0300

    mod carts&profiles

--- database/migrations/2025_12_09_051733_create_offers_table.php
+++ database/migrations/2025_12_09_051733_create_offers_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('offers', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('product_id')->constrained('product');
+            $table->foreignId('product_id')->constrained('products');
             $table->timestamps();
         });
     }
