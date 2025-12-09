ab96a056731065bfc5b4fd06c92771ab5a2eb346
ameerdwdev <dawaraameer14@gmail.com>
2025-12-09 11:18:04 +0400

    finish relationship

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+     public function orderItem(){
+        return $this->hasMany(OrderItem::class);
+    }
+
+     public function payment(){
+        return $this->hasOne(Payment::class);
+    }
 }
