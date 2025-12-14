26145e105948c729fa7968c637522fcc0cc658ca
Ghaieth Kahel <ghaiethka@gmail.com>
2025-12-14 10:48:49 +0300

    offer

--- database/migrations/2025_12_09_051733_create_offers_table.php
+++ database/migrations/2025_12_09_051733_create_offers_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('offers', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id)->constrained('users);
+            $table->foreignId('user_id')->constrained();
             $table->foreignId('product_id')->constrained('products');
             $table->float('discount_percentage')->nullable();  
             $table->float('discount_price')->nullable();      
