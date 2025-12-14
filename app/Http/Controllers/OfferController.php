b92a37755e0ad8fc26d95c361f48e03386bd3568
ameerdwdev <dawaraameer14@gmail.com>
2025-12-14 11:48:04 +0400

    noe

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -1,10 +1,74 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use app\Models\Offer;
 use Illuminate\Http\Request;
 
 class OfferController extends Controller
-{
-    //
+{    
+    
+    //  عرض جميع العروض
+    public function index()
+    {
+        return response()->json(
+            Offer::with('product')->get()
+        );
+    }
+    // عرض عرض واحد
+    public function show($id)
+    {
+        $offer = Offer::with('product')->findOrFail($id);
+        return response()->json($offer);
+    }
+
+    //create offer 
+     public function store(Request $request)
+    {
+        $data = $request->validate([
+            'product_id' => 'required|exists:products,id',
+            'discount_percentage' => 'nullable|numeric|min:0|max:100',
+            'discount_price' => 'nullable|numeric|min:0',
+            'starts_at' => 'nullable|date',
+            'ends_at' => 'nullable|date|after_or_equal:starts_at',
+            'is_active' => 'boolean',
+        ]);
+        $offer = Offer::create($data);
+
+        return response()->json($offer, 201);
+    }
+  
+
+    //  تحديث العرض
+    public function update(Request $request, $id)
+    {
+        $offer = Offer::findOrFail($id);
+
+        $data = $request->validate([
+            'product_id' => 'sometimes|exists:products,id',
+            'discount_percentage' => 'nullable|numeric|min:0|max:100',
+            'discount_price' => 'nullable|numeric|min:0',
+            'starts_at' => 'nullable|date',
+            'ends_at' => 'nullable|date|after_or_equal:starts_at',
+            'is_active' => 'boolean',
+        ]);
+
+        $offer->update($data);
+
+        return response()->json($offer);
+    }
+
+    
+    //  حذف العرض
+    public function destroy($id)
+    {
+        Offer::findOrFail($id)->delete();
+
+        return response()->json([
+            'message' => 'تم حذف العرض بنجاح'
+        ]);
+    }
 }
+
+
+    
+
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,15 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OfferController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
-Route::post('/logout',[AuthController::class,'logout']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::apiResource('offers', OfferController::class);
