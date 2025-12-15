16614bfa45468aae56ea4a2e88986642e5a75a61
ameerdwdev <dawaraameer14@gmail.com>
2025-12-15 12:46:43 +0400

    ameer

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,13 +3,18 @@
 namespace App\Providers;
 
 use App\Models\Cart;
+use App\Models\Offer;
+use App\Policies\CartItemPolicy;
 use App\Policies\CartPolicy;
+use App\Policies\OfferPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
     protected $policies = [
         Cart::class => CartPolicy::class,
+        Offer::class=>OfferPolicy::class,
+        Cart::class=>CartItemPolicy::class,
     ];
 
     public function boot(): void
