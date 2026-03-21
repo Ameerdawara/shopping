492b65e5dae423244546ac91ac42cd78bea8481f
ameerdwdev <dawaraameer14@gmail.com>
2026-03-21 04:44:27 +0400

    b

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,9 +21,9 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        // if (config('app.env') !== 'local') {
-        //     URL::forceScheme('https');
-        // }
+        if (config('app.env') === 'production') {
+        URL::forceScheme('https');
+    }
     }
     
 }
